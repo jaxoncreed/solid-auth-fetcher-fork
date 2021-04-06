d06dfa1187102aa02088ba0512fe5c03087ae471
jaxoncreed <jaxoncreed@gmail.com>
2021-04-06 14:40:42 -0400

    Increased length of random crypto in src

--- src/jose/IsomorphicJoseUtility.ts
+++ src/jose/IsomorphicJoseUtility.ts
@@ -73,7 +73,7 @@ export default class IsomorphicJoseUtility implements IJoseUtility {
   }
 
   async generateCodeVerifier(): Promise<string> {
-    return randomString({ length: 10, type: "base64" });
+    return randomString({ length: 200, type: "base64" });
   }
 
   async generateCodeChallenge(verifier: string): Promise<string> {
